8b2d5349a64eb5bd3cbbc37e8ae54f711365a12d
zerohopex <a584171811@gmail.com>
2019-06-07 09:40:25 +0800

    管理员实现对用户的一系列操作

---
+++ src/main/java/com/dev/core/action/StuInfAction.java
@@ -0,0 +1,19 @@
+package com.dev.core.action;
+
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ResultPath;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Scope("prototype")
+@Namespace("/")
+@ResultPath("/")
+public class StuInfAction extends BasicAction{
+
+
+
+
+
+
+}
