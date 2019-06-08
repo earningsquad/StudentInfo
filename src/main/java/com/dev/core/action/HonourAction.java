4b3a0f47b7c3913aba8c6948644ba014eba79e21
maniacsa <hjm2015@163.com>
2019-06-08 18:01:10 +0800

    修改查询

--- src/main/java/com/dev/core/action/HonourAction.java
+++ src/main/java/com/dev/core/action/HonourAction.java
@@ -53,7 +53,7 @@ public class HonourAction extends BasicAction{
     public String findHonour(){
         Honour honour = new Honour(getType(),getName(),getDetail());
         List<Honour> honourList = new ArrayList<>();
-        honourList = honourService.findHonourBy(honour);
+        honourList = honourService.findHonourBy(honour,1);
         honourResult.success(honourList,honourList.size());
         return SUCCESS;
     }
